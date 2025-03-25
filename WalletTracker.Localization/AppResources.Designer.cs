1749b99be7411cec762a747601305b5c145cdac3
Edgar Allan <edgar.gacayan@gmail.com>
2025-03-25 14:07:21 +0800

    Update resx

--- WalletTracker.Localization/AppResources.Designer.cs
+++ WalletTracker.Localization/AppResources.Designer.cs
@@ -61,11 +61,110 @@ namespace WalletTracker.Localization {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to New.
+        /// </summary>
+        public static string Btn_New {
+            get {
+                return ResourceManager.GetString("Btn_New", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Search.
+        /// </summary>
+        public static string Btn_Search {
+            get {
+                return ResourceManager.GetString("Btn_Search", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to View More.
+        /// </summary>
+        public static string Btn_ViewMore {
+            get {
+                return ResourceManager.GetString("Btn_ViewMore", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} Wallet.
+        /// </summary>
+        public static string Lbl_CurrentMonthWallet {
+            get {
+                return ResourceManager.GetString("Lbl_CurrentMonthWallet", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Good day!.
         /// </summary>
         public static string Lbl_Goodday {
             get {
                 return ResourceManager.GetString("Lbl_Goodday", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No available transactions.
+        /// </summary>
+        public static string Lbl_NoAvailableTransactions {
+            get {
+                return ResourceManager.GetString("Lbl_NoAvailableTransactions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Search any keyword.
+        /// </summary>
+        public static string Lbl_Placeholder_SearchAnyKeyword {
+            get {
+                return ResourceManager.GetString("Lbl_Placeholder_SearchAnyKeyword", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Statistics.
+        /// </summary>
+        public static string Lbl_Statistics {
+            get {
+                return ResourceManager.GetString("Lbl_Statistics", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total Expense:.
+        /// </summary>
+        public static string Lbl_TotalExpense {
+            get {
+                return ResourceManager.GetString("Lbl_TotalExpense", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total Income:.
+        /// </summary>
+        public static string Lbl_TotalIncome {
+            get {
+                return ResourceManager.GetString("Lbl_TotalIncome", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Wallet Activities.
+        /// </summary>
+        public static string Lbl_WalletActivities {
+            get {
+                return ResourceManager.GetString("Lbl_WalletActivities", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Wallet Transactions.
+        /// </summary>
+        public static string Lbl_WalletTransactions {
+            get {
+                return ResourceManager.GetString("Lbl_WalletTransactions", resourceCulture);
             }
         }
     }
