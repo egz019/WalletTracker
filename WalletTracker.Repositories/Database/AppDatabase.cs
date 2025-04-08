0fb6634ed7ede16bc5ad7143ce53c16da7227849
Edgar Allan Gacayan <eallangacayan@discoveryparks.com.au>
2025-04-08 11:50:47 +0800

    add validation

--- WalletTracker.Repositories/Database/AppDatabase.cs
+++ WalletTracker.Repositories/Database/AppDatabase.cs
@@ -5,15 +5,15 @@ public class AppDatabase : MobileDatabase, IAppDatabase
 {
     public AppDatabase() : base("de7f666f-de78-43a1-b31c-bat.db3")
     {
-        _dbAsyncConnection.CreateTableAsync<PreferenceDto>();
-        _dbAsyncConnection.CreateTableAsync<BudgetTypesDto>();
-        _dbAsyncConnection.CreateTableAsync<BudgetSubTypeDto>();
-        _dbAsyncConnection.CreateTableAsync<WalletTransactionsDto>();
+        // _dbAsyncConnection.CreateTableAsync<PreferenceDto>();
+        // _dbAsyncConnection.CreateTableAsync<BudgetTypesDto>();
+        // _dbAsyncConnection.CreateTableAsync<BudgetSubTypeDto>();
+        // _dbAsyncConnection.CreateTableAsync<WalletTransactionsDto>();
 
-        // _dbConnection.CreateTable<BudgetTypesDto>();
-        // _dbConnection.CreateTable<BudgetSubTypeDto>();
-        // _dbConnection.CreateTable<WalletTransactionsDto>();
-        // _dbConnection.CreateTable<PreferenceDto>();
+        _dbConnection.CreateTable<BudgetTypesDto>();
+        _dbConnection.CreateTable<BudgetSubTypeDto>();
+        _dbConnection.CreateTable<WalletTransactionsDto>();
+        _dbConnection.CreateTable<PreferenceDto>();
     }
 
     public async Task InitializeTables()
